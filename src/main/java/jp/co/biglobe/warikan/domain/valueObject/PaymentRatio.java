c15529e8a68f874ca805ac9d47417b252ba4ad00
kubota-akihiro <a-kubota@cnt.biglobe.co.jp>
2022-02-01 13:56:01 +0900

    変数のアクセス範囲を修正しました

--- src/main/java/jp/co/biglobe/warikan/domain/valueObject/PaymentRatio.java
+++ src/main/java/jp/co/biglobe/warikan/domain/valueObject/PaymentRatio.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public class PaymentRatio {
-    public double largeRatio = 1.2;
-    public double MediumRatio = 1;
-    public double smallRatio = 0.8;
+    private double largeRatio = 1.2;
+    private double MediumRatio = 1;
+    private double smallRatio = 0.8;
 
     public PaymentRatio() {
     }
