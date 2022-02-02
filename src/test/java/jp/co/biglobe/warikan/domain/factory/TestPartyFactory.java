2fe72003d4584e0f5ecdb3f33f2e097d02cb3d05
kubota-akihiro <a-kubota@cnt.biglobe.co.jp>
2022-02-02 11:51:24 +0900

    テストのメソッドを修正しました

--- src/test/java/jp/co/biglobe/warikan/domain/factory/TestPartyFactory.java
+++ src/test/java/jp/co/biglobe/warikan/domain/factory/TestPartyFactory.java
@@ -1,7 +1,6 @@
-package jp.co.biglobe.warikan.domain;
+package jp.co.biglobe.warikan.domain.factory;
 
 import jp.co.biglobe.warikan.domain.entity.Party;
-import jp.co.biglobe.warikan.domain.factory.IPartyFactory;
 import jp.co.biglobe.warikan.domain.valueObject.PartyId;
 import jp.co.biglobe.warikan.domain.valueObject.PaymentRatio;
 
--- src/test/java/jp/co/biglobe/warikan/service/PartyApplicationServiceTest.java
+++ src/test/java/jp/co/biglobe/warikan/service/PartyApplicationServiceTest.java
@@ -2,7 +2,7 @@ package jp.co.biglobe.warikan.service;
 
 
 import jp.co.biglobe.warikan.datasource.TestPartyRepository;
-import jp.co.biglobe.warikan.domain.TestPartyFactory;
+import jp.co.biglobe.warikan.domain.factory.TestPartyFactory;
 import jp.co.biglobe.warikan.domain.dto.PaymentModel;
 import org.junit.jupiter.api.Test;
 
@@ -14,16 +14,26 @@ public class PartyApplicationServiceTest {
     @Test
     public void calculatePayment() {
         partyApplicationService.registerParty(1.2, 1.0, 0.8);
-        PaymentModel paymentModel = partyApplicationService.calculatePayment(1, 1, 2, 0, 20000);
-        assertEquals(7500, paymentModel.getPaymentOfLargeMember());
-        assertEquals(6250, paymentModel.getPaymentOfMediumMember());
-        assertEquals(0, paymentModel.getBalanceDue());
+        partyApplicationService.registerParty(1.2, 1.0, 0.8);
+
+        assertCalculatePayment(1, 1, 2, 0, 20000,
+                7500, 6250, 5000, 0);
+        assertCalculatePayment(2, 1, 2, 0, 20000,
+                7500, 6250, 5000, 0);
+//        assertCalculatePayment(3, 1, 2, 0, 20000,
+//                7500, 6250, 5000, 0);
 
 
-        assertEquals(2, 2);
-        assertEquals(3, 3);
-        assertEquals(4, 4);
+    }
 
+    private void assertCalculatePayment(int partyId, int largeMembersNum, int mediumMembersNum, int smallMembersNum, int billingAmount,
+                                        int expectedPaymentOfMediumMember, int expectedPaymentOfLargeMember, int expectedPaymentOfSmallMember, int expectedBalanceDue) {
+
+        PaymentModel paymentModel = partyApplicationService.calculatePayment(partyId, largeMembersNum, mediumMembersNum, smallMembersNum, billingAmount);
+        assertEquals(expectedPaymentOfMediumMember, paymentModel.getPaymentOfLargeMember());
+        assertEquals(expectedPaymentOfLargeMember, paymentModel.getPaymentOfMediumMember());
+        assertEquals(expectedPaymentOfSmallMember, paymentModel.getPaymentOfSmallMember());
+        assertEquals(expectedBalanceDue, paymentModel.getBalanceDue());
     }
 
 }
