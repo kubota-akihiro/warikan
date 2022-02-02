1ca6c63108c0731b1456f9d3b839aa6044e4c79f
kubota-akihiro <a-kubota@cnt.biglobe.co.jp>
2022-02-02 16:02:43 +0900

    テストパターンを少し足した

--- src/test/java/jp/co/biglobe/warikan/service/PartyApplicationServiceTest.java
+++ src/test/java/jp/co/biglobe/warikan/service/PartyApplicationServiceTest.java
@@ -15,25 +15,34 @@ public class PartyApplicationServiceTest {
     public void calculatePayment() {
         partyApplicationService.registerParty(1.2, 1.0, 0.8);
         partyApplicationService.registerParty(1.2, 1.0, 0.8);
-
         assertCalculatePayment(1, 1, 2, 0, 20000,
                 7500, 6250, 5000, 0);
         assertCalculatePayment(2, 1, 2, 0, 20000,
                 7500, 6250, 5000, 0);
+//        //存在しないIDを入力してエラーになるパターン
 //        assertCalculatePayment(3, 1, 2, 0, 20000,
 //                7500, 6250, 5000, 0);
         assertCalculatePayment(1, 1, 2, 2, 30000,
                 7500, 6250, 5000, 0);
+        assertCalculatePayment(1, 1, 2, 2, 35000,
+                8750, 7292, 5834, -2);
+
+        partyApplicationService.registerParty(1.0, 1.0, 1.0);
+        assertCalculatePayment(3, 5, 2, 3, 30000,
+                3000, 3000, 3000, 0);
+        partyApplicationService.registerParty(1.5, 1.0, 0.5);
+        assertCalculatePayment(4, 1, 1, 1, 30000,
+                15000, 10000, 5000, 0);
 
 
     }
 
     private void assertCalculatePayment(int partyId, int largeMembersNum, int mediumMembersNum, int smallMembersNum, int billingAmount,
-                                        int expectedPaymentOfMediumMember, int expectedPaymentOfLargeMember, int expectedPaymentOfSmallMember, int expectedBalanceDue) {
+                                        int expectedPaymentOfLargeMember, int expectedPaymentOfMediumMember, int expectedPaymentOfSmallMember, int expectedBalanceDue) {
 
         PaymentModel paymentModel = partyApplicationService.calculatePayment(partyId, largeMembersNum, mediumMembersNum, smallMembersNum, billingAmount);
-        assertEquals(expectedPaymentOfMediumMember, paymentModel.getPaymentOfLargeMember());
-        assertEquals(expectedPaymentOfLargeMember, paymentModel.getPaymentOfMediumMember());
+        assertEquals(expectedPaymentOfLargeMember, paymentModel.getPaymentOfLargeMember());
+        assertEquals(expectedPaymentOfMediumMember, paymentModel.getPaymentOfMediumMember());
         assertEquals(expectedPaymentOfSmallMember, paymentModel.getPaymentOfSmallMember());
         assertEquals(expectedBalanceDue, paymentModel.getBalanceDue());
     }
