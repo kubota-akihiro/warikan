b2c9b33532f5fa7a45365dcd3fb9f1a69dfa2515
kubota-akihiro <a-kubota@cnt.biglobe.co.jp>
2022-02-08 09:08:41 +0900

    不要なimportを消しました

--- src/main/java/jp/co/biglobe/warikan/domain/payment/valueObject/BalanceDue.java
+++ src/main/java/jp/co/biglobe/warikan/domain/payment/valueObject/BalanceDue.java
@@ -3,9 +3,6 @@ package jp.co.biglobe.warikan.domain.payment.valueObject;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import static java.lang.Math.round;
-
-
 @AllArgsConstructor
 @Getter
 public class BalanceDue {
